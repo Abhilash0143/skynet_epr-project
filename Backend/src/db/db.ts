5a04344475b8c6d57aede81afd0d76d1ca88ad3e
Abhilash S <samratgowda2002@gmail.com>
2026-03-16 23:52:58 +0530

    env update

--- Backend/src/db/db.ts
+++ Backend/src/db/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   user: "postgres",
-  password: "AbhilashDB",
+  password: process.env.DATABASE_KEY,
   host: "localhost",
   port: 5432,
   database: "skynet_epr"
